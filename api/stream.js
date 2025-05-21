cff2f139a7c5f74ef65d6e841234d4251cd7d10f
uxconsultingltd <uxconsultingltd@yahoo.com>
2025-05-21 02:16:03 +0100

    Update stream.js

--- api/stream.js
+++ api/stream.js
@@ -1,15 +1,13 @@
-
 export default async function handler(req, res) {
   const { id } = req.query;
   const url = `http://b3.dinott.com/live/cbfa4abc2f/2da068dcfb39/${id}.m3u8`;
 
   try {
     const response = await fetch(url);
-    const stream = await response.text();
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Content-Type', 'application/vnd.apple.mpegurl');
-    res.status(200).send(stream);
+    res.setHeader('Content-Type', response.headers.get('content-type') || 'application/vnd.apple.mpegurl');
+    response.body.pipe(res);
   } catch (err) {
-    res.status(500).send('Failed to stream');
+    res.status(500).send('Stream proxy failed');
   }
 }
