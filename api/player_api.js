03243064f17d866297339c251f142e0cf238142e
uxconsultingltd <uxconsultingltd@yahoo.com>
2025-05-21 02:58:09 +0100

    Update player_api.js

--- api/player_api.js
+++ api/player_api.js
@@ -1,12 +1,14 @@
 export default async function handler(req, res) {
-  const { username, password, action = 'player_api' } = req.query;
+  const { username, password, action } = req.query;
+
   const url = `http://b3.dinott.com/player_api.php?username=${username}&password=${password}&action=${action}`;
+
   try {
-    const r = await fetch(url);
-    const data = await r.text();
+    const response = await fetch(url);
+    const json = await response.json();
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.status(200).send(data);
+    res.status(200).json(json);
   } catch (err) {
-    res.status(500).json({ error: 'Failed to fetch data from IPTV server' });
+    res.status(500).json({ error: 'Failed to fetch IPTV data' });
   }
 }
